1567c14b7cb9c4bfd37b0bd5fee686d0287e8fbf
caishaodong <1146631758@qq.com>
2020-09-07 13:17:54 +0800

    fixed

--- src/main/java/com/dong/empty/global/base/BaseController.java
+++ src/main/java/com/dong/empty/global/base/BaseController.java
@@ -24,6 +24,6 @@ public class BaseController {
     }
 
     public ResponseResult error(BusinessEnum businessEnum) {
-        return error(businessEnum);
+        return ResponseResult.error(businessEnum);
     }
 }
